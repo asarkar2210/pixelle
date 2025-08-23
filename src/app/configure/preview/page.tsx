205a5d85b779589f2d278cc4746608d8a39bda5f
Ayushman Sarkar <ayushhmansarkar@gmail.com>
2025-08-23 16:38:53 +0530

    Fixed a small bug that arised during deployment

--- src/app/configure/preview/page.tsx
+++ src/app/configure/preview/page.tsx
@@ -4,34 +4,26 @@ import React from 'react'
 import DesignPreview from './DesignPreview'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 
-interface PageProps  {
-    searchParams : {
-        [key: string] : string | string[] | undefined
-    }
-}
-
-const page = async ({searchParams} : PageProps) => {
-
-    const {id} = await searchParams
+export default async function Page({
+    searchParams,
+}: {
+    searchParams: Promise<Record<string, string | string[] | undefined>>
+}) {
+    const { id } = await searchParams
 
     if (!id || typeof id !== 'string') {
         return notFound()
     }
 
-    const configuration = await db.configuration.findUnique({
-        where: {id},
-    })
+    const configuration = await db.configuration.findUnique({ where: { id } })
 
     if (!configuration) {
         return notFound()
     }
 
-  return (
-    <MaxWidthWrapper>
-        <DesignPreview configuration={configuration} />
-    </MaxWidthWrapper>
-    
-  )
+    return (
+        <MaxWidthWrapper>
+            <DesignPreview configuration={configuration} />
+        </MaxWidthWrapper>
+    )
 }
-
-export default page
