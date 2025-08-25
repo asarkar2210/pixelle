9eca484bdab801feff6cb3428f947ab221e4a2a9
Ayushman Sarkar <ayushhmansarkar@gmail.com>
2025-08-25 13:53:14 +0530

    Added more phone options

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -12,32 +12,64 @@ export const MODELS = {
     name: 'models',
     options: [
         {
-            label: 'iPhone X',
-            value: 'iphonex'
-        },
-        {
-            label: 'iPhone 11',
-            value: 'iphone11'
-        },
-        {
-            label: 'iPhone 12',
-            value: 'iphone12'
-        },
-        {
             label: 'iPhone 13',
             value: 'iphone13'
+        },
+        {
+            label: 'iPhone 13 Pro',
+            value: 'iphone13pro'
+        },
+        {
+            label: 'iPhone 13 Pro Max',
+            value: 'iphone13promax'
         },
         {
             label: 'iPhone 14',
             value: 'iphone14'
         },
         {
+            label: 'iPhone 14 Plus',
+            value: 'iphone14plus'
+        },
+        {
+            label: 'iPhone 14 Pro',
+            value: 'iphone14pro'
+        },
+        {
+            label: 'iPhone 14 Pro Max',
+            value: 'iphone14promax'
+        },
+        {
             label: 'iPhone 15',
             value: 'iphone15'
         },
         {
+            label: 'iPhone 15 Plus',
+            value: 'iphone15plus'
+        },
+        {
+            label: 'iPhone 15 Pro',
+            value: 'iphone15pro'
+        },
+        {
+            label: 'iPhone 15 Pro Max',
+            value: 'iphone15promax'
+        },
+        {
             label: 'iPhone 16',
             value: 'iphone16'
+        },
+        {
+            label: 'iPhone 16 Plus',
+            value: 'iphone16plus'
+        },
+        {
+            label: 'iPhone 16 Pro',
+            value: 'iphone16pro'
+        },
+        {
+            label: 'iPhone 16 Pro Max',
+            value: 'iphone16promax'
         },
     ]
 } as const
